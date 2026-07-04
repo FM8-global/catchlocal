a077b2beccdd785cf14002ca9ecf258a7ecc68ed
FM8-global <info@fm8.global>
2026-07-04 15:21:17 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "catchlocal-v22";
+const CACHE = "catchlocal-v23";
 const ASSETS = ["./","./index.html","./manifest.webmanifest",
   "./icon-192.png","./icon-512.png","./icon-maskable-512.png","./apple-touch-icon.png"];
 self.addEventListener("install", e => {
