9a0e2e15ef3da7f0b96590d676b1801b684c9f21
FM8-global <info@fm8.global>
2026-07-25 14:21:39 +1000

    Add files via upload

---
+++ catchlocal-v24-deploy/sw.js
@@ -0,0 +1,32 @@
+const CACHE = "catchlocal-v36";
+const ASSETS = ["./","./index.html","./manifest.webmanifest",
+  "./icon-192.png","./icon-512.png","./icon-maskable-512.png","./apple-touch-icon.png","./locales/en.json","./locales/fr.json","./locales/es.json","./locales/pt.json","./locales/it.json","./locales/de.json","./locales/vi.json","./locales/zh.json","./locales/ja.json","./locales/ko.json","./locales/th.json","./locales/tl.json"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  const req = e.request;
+  if (req.method !== "GET") return;
+  const isNav = req.mode === "navigate" || (req.headers.get("accept") || "").includes("text/html");
+  if (isNav) {
+    e.respondWith(
+      fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put("./index.html", copy)).catch(()=>{});
+        return res;
+      }).catch(() => caches.match(req).then(h => h || caches.match("./index.html")))
+    );
+  } else {
+    e.respondWith(
+      caches.match(req).then(hit => hit || fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(req, copy)).catch(()=>{});
+        return res;
+      }).catch(()=>undefined))
+    );
+  }
+});
